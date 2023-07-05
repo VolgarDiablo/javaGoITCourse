be072a1eed0f0e52c093112ceb3da25190974b06
tohaddid <didkovskiy.anton@gmail.com>
2023-07-05 16:54:00 +0300

    done

--- Module10/Task_3/src/main/java/org/example/Main.java
+++ Module10/Task_3/src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        String separator = File.separator;
+        String inputFile = "src" + separator + "main" + separator + "resources" + separator + "words.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+
+        } catch (IOException e) {
+            System.out.println("Помилка: Не вдалося зчитати дані з файлу file.txt.");
+            e.printStackTrace();
+        }
 
         for (int i = 1; i <= 5; i++) {
 
